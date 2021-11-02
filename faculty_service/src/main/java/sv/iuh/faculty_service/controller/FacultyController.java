d56b4bc3b95c308ec493186f1911cfc144d8de1b
Tuan Khang <you@example.com>
2021-11-02 11:32:45 +0700

    clound config

--- faculty_service/src/main/java/sv/iuh/faculty_service/controller/FacultyController.java
+++ faculty_service/src/main/java/sv/iuh/faculty_service/controller/FacultyController.java
@@ -2,6 +2,7 @@ package sv.iuh.faculty_service.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import sv.iuh.faculty_service.entity.Faculty;
 import sv.iuh.faculty_service.service.FacultyService;
@@ -27,8 +28,10 @@ public class FacultyController {
         return facultyService.findById(id);
     }
 
+    @Value("${welcome}")
+    private String name;
     @GetMapping("/")
     public String helloWorld(){
-        return "Hello Khang!";
+        return name;
     }
 }
